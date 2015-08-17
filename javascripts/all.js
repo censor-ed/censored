60587566749231149e4df7ae3194105d1788e7aa
censor-ed <censor-ed@server.dummy>
2015-08-17 10:43:11 +0900

    add filter

--- javascripts/all.js
+++ javascripts/all.js
@@ -2,28 +2,73 @@
   $(function() {
     return new Vue({
       el: "#presets",
+      data: {
+        presets: []
+      },
+      created: function() {
+        return $.getJSON("./presets.json", (function(_this) {
+          return function(json) {
+            return _this.$data.presets = json;
+          };
+        })(this));
+      },
       methods: {
         toggle: function(e) {
-          $(".preset").prop("checked", $(e.target).is(":checked"));
-          return this.update();
+          var i, len, preset, ref, results;
+          ref = this._visiblePresets();
+          results = [];
+          for (i = 0, len = ref.length; i < len; i++) {
+            preset = ref[i];
+            results.push(preset.$set("checked", $(e.target).is(":checked")));
+          }
+          return results;
         },
-        update: function() {
-          return $("#output").val(this._values().join("\n"));
+        values: function() {
+          var i, len, preset, ref, results;
+          ref = this.$data.presets;
+          results = [];
+          for (i = 0, len = ref.length; i < len; i++) {
+            preset = ref[i];
+            if (preset.checked) {
+              results.push(preset.regexp);
+            }
+          }
+          return results;
         },
         focus: function(e) {
           return $(e.target).select();
         },
-        _values: function() {
-          var checked, i, len, ref, results;
-          ref = $(":checked").toArray();
+        truncate: function(strings, size) {
+          if (strings.length > size) {
+            return $.trim(strings).substring(0, size).trim(this) + "...";
+          } else {
+            return strings;
+          }
+        },
+        _visiblePresets: function() {
+          var checkbox, i, len, preset, ref, results, values;
+          values = (function() {
+            var i, len, ref, results;
+            ref = $(":checkbox").toArray();
+            results = [];
+            for (i = 0, len = ref.length; i < len; i++) {
+              checkbox = ref[i];
+              results.push($(checkbox).val());
+            }
+            return results;
+          })();
+          ref = this.$data.presets;
           results = [];
           for (i = 0, len = ref.length; i < len; i++) {
-            checked = ref[i];
-            if ($(checked).val() !== "on") {
-              results.push($(checked).val());
+            preset = ref[i];
+            if (values.indexOf(preset.regexp) > 0) {
+              results.push(preset);
             }
           }
           return results;
+        },
+        checkedSize: function() {
+          return this.values().length;
         }
       }
     });
